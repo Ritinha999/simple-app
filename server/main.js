bdbebddff87251bce34646da36f7abe393e0b2a1
bratelefant <59534030+bratelefant@users.noreply.github.com>
2024-09-04 19:18:50 +0200

    added async

--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
-import { Meteor } from 'meteor/meteor';
-import { Accounts } from 'meteor/accounts-base';
-import { TasksCollection } from '/imports/db/TasksCollection';
-import '/imports/api/TasksMethods';
+import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
+import { TasksCollection } from "/imports/db/TasksCollection";
+import "/imports/api/TasksMethods";
 
 const insertTask = (taskText, user) =>
-  TasksCollection.insert({
+  TasksCollection.insertAsync({
     text: taskText,
     userId: user._id,
     createdAt: new Date(),
@@ -15,7 +15,7 @@ const SEED_PASSWORD = "password";
 
 Meteor.startup(async () => {
   if (!Accounts.findUserByUsername(SEED_USERNAME)) {
-    Accounts.createUser({
+    Accounts.createUserAsync({
       username: SEED_USERNAME,
       password: SEED_PASSWORD,
     });
