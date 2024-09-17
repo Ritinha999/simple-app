32516415a5b36545c55eaa9c0ff556fc2ed662d8
bratelefant <59534030+bratelefant@users.noreply.github.com>
2024-09-17 16:37:55 +0200

    prettier code

--- imports/ui/Bewertung.jsx
+++ imports/ui/Bewertung.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 export const Bewertung = ({ label }) => {
   const [value, setValue] = React.useState(2);
