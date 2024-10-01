793dc3386693c1e1ad04006670c96cb9ce6bd0e4
Rita Campos <ritanogueiracampos@gmail.com>
2024-10-01 17:37:35 +0200

    Darkmode

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -5,16 +5,27 @@ import { LoginForm } from "./LoginForm";
 import { Box } from "@mui/material";
 import { TaskManager } from "./TaskManager";
 import { MenuBar } from "./MenuBar";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const App = () => {
   const user = useTracker(() => Meteor.user());
-  
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
+
   return (
-    <div className="app">
-      <MenuBar>Rita's To-Do Liste</MenuBar>
-      <Box className="main" sx={{ padding: 4 }}>
-        {user ? <TaskManager/> : <LoginForm />}
+    <ThemeProvider theme={darkTheme}>
+      <Box>
+        <CssBaseline />
+        <MenuBar>Rita's To-Do Liste</MenuBar>
+        <Box sx={{ padding: 4 }}>
+          {user ? <TaskManager /> : <LoginForm />}
+        </Box>
       </Box>
-    </div>
+    </ThemeProvider>
   );
 };
