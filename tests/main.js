10e07031df9fee1d73019e621b092c83047f769a
bratelefant <59534030+bratelefant@users.noreply.github.com>
2024-11-05 13:37:56 +0100

    method tests

--- tests/main.js
+++ tests/main.js
@@ -1,4 +1,5 @@
 import assert from "assert";
+import "../imports/collections/categories/server/tests";
 
 describe("simple-app", function () {
   it("package.json has correct name", async function () {
